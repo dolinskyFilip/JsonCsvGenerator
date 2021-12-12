975e51b31c2ee3e627c69df325ecb2a7769eec88
filip <filipdolinski@gmail.com>
2021-12-12 22:27:58 +0100

    Assertions added

--- JsonGenerator/src/test/java/io/cardinality/jsonapp/integration/PositionControllerIT.java
+++ JsonGenerator/src/test/java/io/cardinality/jsonapp/integration/PositionControllerIT.java
@@ -1,8 +1,11 @@
 package io.cardinality.jsonapp.integration;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cardinality.jsonapp.JsonappApplication;
+import io.cardinality.jsonapp.model.FlatPosition;
+import io.cardinality.jsonapp.model.Position;
 import io.cardinality.jsonapp.web.advice.ErrorResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
@@ -11,12 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -63,7 +66,10 @@ public class PositionControllerIT {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn().getResponse();
 
+        var positions = mapper.readValue(result.getContentAsString(), new TypeReference<List<Position>>(){});
 
+        Assertions.assertThat(positions).isNotEmpty();
+        Assertions.assertThat(positions).hasSize(4);
     }
 
     @Test
@@ -96,6 +102,11 @@ public class PositionControllerIT {
                 .andExpect(content().contentType("application/json"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn().getResponse();
+
+        var flatPositions = mapper.readValue(result.getContentAsString(), new TypeReference<List<FlatPosition>>(){});
+        Assertions.assertThat(flatPositions).isNotEmpty();
+        Assertions.assertThat(flatPositions).hasSize(4);
+
     }
 
     @Test
